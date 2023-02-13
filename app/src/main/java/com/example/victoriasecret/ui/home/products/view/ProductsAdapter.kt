bd1b7cf348942ea3e88eb02795cc0fe6abd1462e
Apurba <apurva.gouda@gmail.com>
2023-02-13 10:48:52 +0530

    recyclerview and adapter setup

--- app/src/main/java/com/example/victoriasecret/ui/home/products/view/ProductsAdapter.kt
+++ app/src/main/java/com/example/victoriasecret/ui/home/products/view/ProductsAdapter.kt
@@ -1,8 +1,12 @@
 package com.example.victoriasecret.ui.home.products.view
 
-import android.view.View
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.victoriasecret.R
+import com.example.victoriasecret.databinding.ItemProductBinding
+import com.example.victoriasecret.extension.loadImage
 import com.example.victoriasecret.ui.home.products.data.models.Product
 
 class ProductsAdapter(
@@ -14,18 +18,44 @@ class ProductsAdapter(
     notifyDataSetChanged()
   }
 
-  inner class ProductVH(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-  }
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductVH {
-
+    val viewBinding = ItemProductBinding.inflate(LayoutInflater.from(parent.context))
+    return ProductVH(viewBinding)
   }
 
   override fun onBindViewHolder(holder: ProductVH, position: Int) {
-
+    holder.bindData(productList[position])
   }
 
   override fun getItemCount(): Int = productList.size
+
+
+  inner class ProductVH(private val binding: ItemProductBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    init {
+      binding.root.setOnClickListener {
+        if (adapterPosition == RecyclerView.NO_POSITION) return@setOnClickListener
+        //Register Listener here
+//        itemChannel.trySend(feeds[adapterPosition].id)
+      }
+
+    }
+
+    fun bindData(product: Product) {
+      binding.apply {
+        productName.text = product.name
+        productDesc.text = product.productDesc
+        offerPrice.text = getPrice(itemView.context,product.offerPrice)
+        price.text = getPrice(itemView.context,product.price)
+        productIV.loadImage(product.productUrl)
+
+      }
+    }
+
+    private fun getPrice(context: Context, price:String) :String{
+      return context.resources.getString(R.string.price,price)
+    }
+  }
 
 }
--- app/src/main/java/com/example/victoriasecret/ui/home/products/view/ProductsFragment.kt
+++ app/src/main/java/com/example/victoriasecret/ui/home/products/view/ProductsFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.victoriasecret.R
+import com.example.victoriasecret.databinding.FragmentProductsBinding
 import com.example.victoriasecret.ui.home.products.ProductsVMBuilder
 
 class ProductsFragment : Fragment() {
-
+  private lateinit var productFragmentBinding: FragmentProductsBinding
   private lateinit var vm: ProductsViewModel
   private val adapter: ProductsAdapter by lazy {
     ProductsAdapter(emptyList())
@@ -18,20 +21,27 @@ class ProductsFragment : Fragment() {
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
+
+    productFragmentBinding = FragmentProductsBinding.inflate(layoutInflater)
+
     vm = ProductsVMBuilder.build(this)
+    vm.fetchProducts()
     setUpObservers()
     setUpRecyclerView()
-    return inflater.inflate(R.layout.fragment_products, container, false)
+    return productFragmentBinding.root
   }
 
-  fun setUpRecyclerView() {
-    //Todo Set Recyclerview
+  private fun setUpRecyclerView() {
+    productFragmentBinding.productList.apply {
+      layoutManager =
+        LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+      adapter = adapter
+    }
   }
 
   private fun setUpObservers() {
     vm.productsStream.observe(viewLifecycleOwner) {
       if (it.isNullOrEmpty()) return@observe
-
       adapter.updateProducts(it)
     }
   }
