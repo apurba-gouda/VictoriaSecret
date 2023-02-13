a59ec997a62e1973c57dcde4f2165d499c3dc9b9
Apurba <apurva.gouda@gmail.com>
2023-02-13 09:46:04 +0530

    update branch

--- app/src/main/java/com/example/victoriasecret/ui/home/products/ProductsFragment.kt
+++ app/src/main/java/com/example/victoriasecret/ui/home/products/ProductsFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.example.victoriasecret.ui.home.products.data.http.HttpProductsService
+import com.example.victoriasecret.R
 import kotlinx.coroutines.launch
 
 class ProductsFragment : Fragment() {
@@ -27,16 +27,10 @@ class ProductsFragment : Fragment() {
     super.onResume()
     lifecycleScope.launch {
       try {
-        val res = getProducts()
-        Log.i(TAG, "onResume: Res: $res")
       } catch (e: Exception) {
         Log.i(TAG, "onResume: Exception : ${e.message}")
       }
     }
-  }
-
-  private suspend fun getProducts() {
-    HttpProductsService().getProducts()
   }
 
   companion object {
